21a561a18f8276b0a3c8d8ff155f0f32fc426752
EKI-INDRADI <eki.indradi@gmail.com>
2022-12-25 16:11:07 +0700

    TYPOGRAPHY, CUSTOM FONT, CUSTOM COLORS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     extend: {  // untuk menambahkan aturan2 yang tidak ada pada auto complite
       spacing: {  // https://tailwindcss.com/docs/customizing-spacing#extending-the-default-spacing-scale
         '13': '3.25rem'
+      },
+      fontFamily : {
+        inter : ['Inter']
+      },
+      colors: {
+        wpu : '#bada55',
+        kopi : '#c0ffee'
       }
+    
     },
   },
   plugins: [],
