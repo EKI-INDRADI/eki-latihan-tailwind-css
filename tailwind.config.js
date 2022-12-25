1d17a4832480e9c19affc25315aa696e7fb98d1f
EKI-INDRADI <eki.indradi@gmail.com>
2022-12-25 15:27:19 +0700

    SPACING N CONFIG

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}",  "./index.html"],
   theme: {
-    extend: {},
+    extend: {  // untuk menambahkan aturan2 yang tidak ada pada auto complite
+      spacing: {  // https://tailwindcss.com/docs/customizing-spacing#extending-the-default-spacing-scale
+        '13': '3.25rem'
+      }
+    },
   },
   plugins: [],
 }
